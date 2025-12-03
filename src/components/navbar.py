ab06a54a26f1293a59b7a92300825fe99f45ede7
Elias Lahlouh <elias@MacBook-Air-de-Elias.local>
2025-12-03 13:20:18 +0100

    DOCTORS-4 | feat: update clean architecture

--- src/components/navbar.py
+++ src/components/navbar.py
@@ -5,9 +5,6 @@ def create_navbar():
     return html.Nav([
         html.Div([
             html.A("Doctors Dashboard", href="/", className='navbar-brand'),
-            html.Ul([
-                html.Li(html.A("Accueil", href="/", className='nav-link')),
-            ], className='navbar-nav')
         ], style={
             'display': 'flex',
             'justifyContent': 'space-between',
